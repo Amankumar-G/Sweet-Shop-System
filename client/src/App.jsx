e7dd96172cdcbabda07d089e348f7d0c112f9bc1
Amankumar-G <amangaloliya@gmail.com>
2025-09-20 16:23:48 +0530

    chore(frontend): setup ShadCN configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { Button } from "@/components/ui/button"
 import './App.css'
 
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+       <Button>Click me</Button>
     </h1>
   )
 }
